0a693118e87c96d195030be3ac126deb36c94084
Julio Muller <juliodolago@gmail.com>
2022-06-10 00:27:46 -0300

    refactor: move source code to folder "src/"

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Dashgo</title>
+      </Head>
+      <h1>Hello, there!</h1>;
+    </>
+  );
+}
