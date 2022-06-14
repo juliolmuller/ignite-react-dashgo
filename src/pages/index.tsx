554552e14e60013f937d3314b20c6aea16133ae4
Julio Muller <juliodolago@gmail.com>
2022-06-13 22:47:49 -0300

    feat: create 'SignInPage'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,64 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
 import Head from 'next/head';
 
-export default function Home() {
+export default function SignInPage() {
   return (
     <>
       <Head>
-        <title>Home | Dashgo</title>
+        <title>Sign In | Dashgo</title>
       </Head>
-      <h1>Hello, there!</h1>
+      <Flex align="center" justify="center" h="100vh" w="100vw">
+        <Flex
+          as="form"
+          flexDir="column"
+          w="100%"
+          maxW={360}
+          borderRadius={8}
+          bg="gray.800"
+          p="8"
+        >
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                size="lg"
+                variant="filled"
+                bg="gray.900"
+                focusBorderColor="pink.500"
+                _hover={{ bg: 'gray.900' }}
+                id="email"
+                name="email"
+                type="email"
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel htmlFor="password">Senha</FormLabel>
+              <Input
+                size="lg"
+                variant="filled"
+                bg="gray.900"
+                focusBorderColor="pink.500"
+                _hover={{ bg: 'gray.900' }}
+                id="password"
+                name="password"
+                type="password"
+              />
+            </FormControl>
+          </Stack>
+
+          <Button size="lg" mt="6" colorScheme="pink" type="submit">
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
     </>
   );
 }
