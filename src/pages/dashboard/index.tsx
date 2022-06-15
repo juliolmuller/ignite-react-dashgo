07d2130cd3d72a72fa739462cbdb333bf1a7807b
Julio Muller <juliodolago@gmail.com>
2022-06-14 22:54:05 -0300

    feat: create sidebar base structure

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
+import { Flex } from '@chakra-ui/react';
 import Head from 'next/head';
 
-import { AppHeader } from '~/components';
+import { AppHeader, AppSideBar } from '~/components';
 
 export default function DashboardPage() {
   return (
@@ -9,7 +10,13 @@ export default function DashboardPage() {
         <title>Dashboard | Dashgo</title>
       </Head>
 
-      <AppHeader />
+      <Flex direction="column" h="100vh">
+        <AppHeader />
+
+        <Flex w="100%" maxW={1480} mx="auto" my="6" px="6">
+          <AppSideBar />
+        </Flex>
+      </Flex>
     </>
   );
 }
