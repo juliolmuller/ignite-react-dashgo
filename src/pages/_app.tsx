c7cf80dec5cb089fe58dc8b7e77425d3b6128803
Julio Muller <juliodolago@gmail.com>
2022-06-30 23:18:51 -0300

    feat: configure MirageJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,15 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 import { DrawerProvider } from '~/contexts';
+import { initializeServer } from '~/services/mirage';
 import theme from '~/styles/theme';
+
+if (
+  typeof window !== 'undefined'
+  // && process.env.NODE_ENV === 'development'
+) {
+  initializeServer();
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import { createServer, Factory, Model } from 'miragejs';
+
+export interface UserModel {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+}
+
+export function initializeServer() {
+  return createServer({
+    models: {
+      user: Model.extend<Partial<UserModel>>({}),
+    },
+
+    factories: {
+      user: Factory.extend({
+        name: () => `${faker.name.firstName()} ${faker.name.lastName()}`,
+        email: () => faker.internet.email().toLowerCase(),
+        password: () => 'Senha123',
+        createdAt: () => faker.date.recent().toISOString(),
+      }),
+    },
+
+    seeds(server) {
+      server.createList('user', 100);
+    },
+
+    routes() {
+      this.timing = 800;
+      this.namespace = 'api';
+
+      this.get('/users');
+      this.post('/users');
+
+      // reset namespace to avoid conflicts with Next's router
+      this.namespace = '';
+      this.passthrough();
+    },
+  });
+}
