162bfb094100209792db70ccb17c11f3f0658c46
Julio Muller <juliodolago@gmail.com>
2022-07-05 01:09:56 -0300

    refactor: move 'mirage' service one level up

--- src/services/mirage.ts
+++ src/services/mirage.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker/locale/pt_BR';
 import { ActiveModelSerializer, createServer, Factory, Model } from 'miragejs';
 
-import { AUTH_BASE_URL } from '../api';
+import { AUTH_BASE_URL } from '~/services/api';
 
 export interface PagedResponse<T> {
   data: T[];
@@ -91,6 +91,7 @@ export function initializeServer() {
 
       // reset namespace to avoid conflicts with Next's router
       this.namespace = '';
+      this.passthrough();
       this.passthrough(`${AUTH_BASE_URL}/**`);
     },
   });
