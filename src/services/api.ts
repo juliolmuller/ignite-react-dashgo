4b7daa53930b338bf5361538ba36218f14df86c5
Julio Muller <juliodolago@gmail.com>
2022-07-09 01:07:07 -0300

    feat: add support to access cookies on server-side

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { GetServerSidePropsContext } from 'next';
 import { parseCookies, setCookie } from 'nookies';
 
 interface RequestsAwaitingTokenRefresh {
@@ -6,92 +7,96 @@ interface RequestsAwaitingTokenRefresh {
   reject: () => void;
 }
 
-// flag to allow queuing requests awaiting token refresh
-let isRefreshingToken = false;
-// queue of requests to be executed once token is refreshed
-const requestsQueue = new Set<RequestsAwaitingTokenRefresh>();
-
 export const AUTH_BASE_URL = process.env.AUTH_SERVER ?? 'http://localhost:3333';
 
-const api = axios.create({
-  baseURL: AUTH_BASE_URL,
-});
+export function createClient(context?: GetServerSidePropsContext) {
+  // flag to allow queuing requests awaiting token refresh
+  let isRefreshingToken = false;
+  // queue of requests to be executed once token is refreshed
+  const requestsQueue = new Set<RequestsAwaitingTokenRefresh>();
 
-// try to add bearer token to all requests
-api.interceptors.request.use((config) => {
-  const cookies = parseCookies();
-  const token = cookies[process.env.NEXT_PUBLIC_COOKIE_KEY_TOKEN!];
+  const httpClient = axios.create({
+    baseURL: AUTH_BASE_URL,
+  });
 
-  if (token) {
-    config.headers!['Authorization'] = `Bearer ${token}`;
-  }
+  // try to add bearer token to all requests
+  httpClient.interceptors.request.use((config) => {
+    const cookies = parseCookies(context);
+    const token = cookies[process.env.NEXT_PUBLIC_COOKIE_KEY_TOKEN!];
 
-  return config;
-});
-
-// try to refresh token on responses status 401
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (
-      error.response?.status !== 401 ||
-      error.response?.data?.code !== 'token.expired'
-    ) {
-      return Promise.reject(error);
+    if (token) {
+      config.headers!['Authorization'] = `Bearer ${token}`;
     }
 
-    if (isRefreshingToken) {
-      return new Promise((resolve, reject) => {
-        requestsQueue.add({
-          resolve: () => resolve(api.request(error.config)),
-          reject: () => reject(error),
+    return config;
+  });
+
+  // try to refresh token on responses status 401
+  httpClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (
+        error.response?.status !== 401 ||
+        error.response?.data?.code !== 'token.expired'
+      ) {
+        return Promise.reject(error);
+      }
+
+      if (isRefreshingToken) {
+        return new Promise((resolve, reject) => {
+          requestsQueue.add({
+            resolve: () => resolve(httpClient.request(error.config)),
+            reject: () => reject(error),
+          });
         });
-      });
-    }
+      }
 
-    const cookies = parseCookies();
-    const refreshToken =
-      cookies[process.env.NEXT_PUBLIC_COOKIE_KEY_REFRESH_TOKEN!];
+      const cookies = parseCookies(context);
+      const refreshToken =
+        cookies[process.env.NEXT_PUBLIC_COOKIE_KEY_REFRESH_TOKEN!];
 
-    if (!refreshToken) {
-      return Promise.reject(error);
-    }
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
 
-    try {
-      isRefreshingToken = true;
-      const { data } = await api.post('refresh', { refreshToken });
-      const cookiesOptions = {
-        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-        path: '/',
-      };
+      try {
+        isRefreshingToken = true;
+        const { data } = await httpClient.post('refresh', { refreshToken });
+        const cookiesOptions = {
+          maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+          path: '/',
+        };
 
-      setCookie(
-        null,
-        process.env.NEXT_PUBLIC_COOKIE_KEY_TOKEN!,
-        data.token,
-        cookiesOptions,
-      );
-      setCookie(
-        null,
-        process.env.NEXT_PUBLIC_COOKIE_KEY_REFRESH_TOKEN!,
-        data.refreshToken,
-        cookiesOptions,
-      );
-      requestsQueue.forEach((request) => {
-        requestsQueue.delete(request);
-        request.resolve();
-      });
+        setCookie(
+          context,
+          process.env.NEXT_PUBLIC_COOKIE_KEY_TOKEN!,
+          data.token,
+          cookiesOptions,
+        );
+        setCookie(
+          context,
+          process.env.NEXT_PUBLIC_COOKIE_KEY_REFRESH_TOKEN!,
+          data.refreshToken,
+          cookiesOptions,
+        );
+        requestsQueue.forEach((request) => {
+          requestsQueue.delete(request);
+          request.resolve();
+        });
 
-      return api.request(error.config);
-    } catch {
-      requestsQueue.forEach((request) => {
-        requestsQueue.delete(request);
-        request.reject();
-      });
+        return httpClient.request(error.config);
+      } catch {
+        requestsQueue.forEach((request) => {
+          requestsQueue.delete(request);
+          request.reject();
+        });
 
-      return Promise.reject(error);
-    }
-  },
-);
+        return Promise.reject(error);
+      }
+    },
+  );
 
-export default api;
+  return httpClient;
+}
+
+export default createClient();
